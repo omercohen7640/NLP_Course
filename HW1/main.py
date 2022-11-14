ee12cfce9350fcfc68e11a41dbe53fec4b3c86b2
nimrod216 <nimrod216@gmail.com>
2022-11-14 14:16:15 +0200

    correct main.py

--- HW1/main.py
+++ HW1/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pickle
+from preprocessing import preprocess_train
+from optimization import get_optimal_vector
+from inference import tag_all_test
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    threshold = 1
+    lam = 1
+
+    train_path = "data/train1.wtag"
+    test_path = "data/comp1.words"
+
+    weights_path = 'weights.pkl'
+    predictions_path = 'predictions.wtag'
+
+    statistics, feature2id = preprocess_train(train_path, threshold)
+    get_optimal_vector(statistics=statistics, feature2id=feature2id, weights_path=weights_path, lam=lam)
+
+    with open(weights_path, 'rb') as f:
+        optimal_params, feature2id = pickle.load(f)
+    pre_trained_weights = optimal_params[0]
+
+    print(pre_trained_weights)
+    tag_all_test(test_path, pre_trained_weights, feature2id, predictions_path)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
