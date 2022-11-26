0d992d3cc0a59b1dcb53afcb1f0089c30666d3c4
omercohen <omercohen7640@gmail.com>
2022-11-26 09:46:18 +0200

    test of train set

--- HW1/main.py
+++ HW1/main.py
@@ -36,24 +36,24 @@ def calc_acc(pred_path, test_path, S):
 
 
 def main():
-    threshold = 1
-    lam = 1
-
-    train_path = "data/train1.wtag"
-    test_path = "data/train1test.wtag"
-
-    weights_path = 'weights.pkl'
-    predictions_path = 'predictions.wtag'
-
-    statistics, feature2id = preprocess_train(train_path, threshold)
-    #get_optimal_vector(statistics=statistics, feature2id=feature2id, weights_path=weights_path, lam=lam)
-
-    with open(weights_path, 'rb') as f:
-        optimal_params, feature2id = pickle.load(f)
-    pre_trained_weights = optimal_params[0]
-
-    print(pre_trained_weights)
-    tag_all_test(test_path, pre_trained_weights, feature2id, statistics, predictions_path)
+    # threshold = 1
+    # lam = 1
+    #
+    # train_path = "data/train1.wtag"
+    # test_path = "data/train1test.wtag"
+    #
+    # weights_path = 'weights.pkl'
+    # predictions_path = 'predictions.wtag'
+    #
+    # statistics, feature2id = preprocess_train(train_path, threshold)
+    # get_optimal_vector(statistics=statistics, feature2id=feature2id, weights_path=weights_path, lam=lam)
+    #
+    # with open(weights_path, 'rb') as f:
+    #     optimal_params, feature2id = pickle.load(f)
+    # pre_trained_weights = optimal_params[0]
+    #
+    # print(pre_trained_weights)
+    # tag_all_test(test_path, pre_trained_weights, feature2id, statistics, predictions_path)
     calc_acc(predictions_path, test_path, statistics.tags)
 
 if __name__ == '__main__':
